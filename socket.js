0c2379a9256f7cf2e1414b2905e4ba2fe858c9d3
rajazara75 <rajazara75@gmail.com>
2021-05-31 16:46:45 +0500

    Video Calling App

---
+++ socket.js
@@ -0,0 +1,60 @@
+const { Server } = require('socket.io');
+let IO;
+
+module.exports.initIO = (httpServer) => {
+    IO = new Server(httpServer);
+
+    IO.use((socket, next) => {
+        if (socket.handshake.query) {
+            let userName = socket.handshake.query.name
+            socket.user = userName;
+            next();
+            console.log(socket.user);
+        }
+    })
+
+    IO.on('connection', (socket) => {
+        console.log(socket.user, "Connected");
+        socket.join(socket.user);
+
+        socket.on('call', (data) => {
+            let callee = data.name;
+            let rtcMessage = data.rtcMessage;
+
+            socket.to(callee).emit("newCall", {
+                caller: socket.user,
+                rtcMessage: rtcMessage
+            })
+
+        })
+
+        socket.on('answerCall', (data) => {
+            let caller = data.caller;
+            rtcMessage = data.rtcMessage
+
+            socket.to(caller).emit("callAnswered", {
+                callee: socket.user,
+                rtcMessage: rtcMessage
+            })
+
+        })
+
+        socket.on('ICEcandidate', (data) => {
+            let otherUser = data.user;
+            let rtcMessage = data.rtcMessage;
+
+            socket.to(otherUser).emit("ICEcandidate", {
+                sender: socket.user,
+                rtcMessage: rtcMessage
+            })
+        })
+    })
+}
+
+module.exports.getIO = () => {
+    if (!IO) {
+        throw Error("IO not initilized.")
+    } else {
+        return IO;
+    }
+}
